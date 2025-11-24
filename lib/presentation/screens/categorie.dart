2b89159c611f66239833e560f6cf48672472592b
josue-dex19 <ananijosue08@gmail.com>
2025-11-24 00:42:29 +0000

    feat: Add bottom navigation and separate screens
    Refactors the `Home` screen into a stateful widget to manage a `BottomNavigationBar`. Introduces separate screens for different sections of the app.
    
    - Converts `Home` from a `StatelessWidget` to a `StatefulWidget` to handle tab selection.
    - Implements a `BottomNavigationBar` with four tabs: "Accueil", "Profil", "Favoris", and "Catégorie".
    - Creates a new `Accueil` screen and moves the original `Home` screen's UI into it.
    - Adds new placeholder screens: `Categorie`, `Favorie`, and `Profil`.

---
+++ lib/presentation/screens/categorie.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Categorie extends StatelessWidget {
+  const Categorie({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -1,129 +1,52 @@
+import 'package:cinebox/presentation/screens/accueil.dart';
+import 'package:cinebox/presentation/screens/profil.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/containerImageCarousel.dart';
 import '../widgets/rowTextIcon.dart';
+import 'categorie.dart';
+import 'favorie.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+  final List<Widget> pages = [Accueil(), Profil(), Favorie(), Categorie()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        toolbarHeight: 100,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 10,
-          children: [
-            Container(alignment: Alignment.topLeft, child: Text('CineHub')),
-            SizedBox(
-              height: 40,
-              child: SearchBar(
-                hintText: "Rechercher un film",
-                leading: Icon(Icons.search),
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: MediaQuery.heightOf(context) / 5),
-            Stack(
-              children: [
-                SizedBox(
-                  height: MediaQuery.heightOf(context) / 2,
-                  child: Image.network(
-                    "https://imgs.search.brave.com/TC2AbDgXCDsk_r0QWrF_yAs7qkgNNopeNaLsr-e-UdM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tL0lD/bkZlMWtlMTB4YUto/MTh3YzlGemFvMTZD/Um1JN0NVR0M5OGZY/TDJkNTVfS2xuZExQ/bmVZbG9vaXMteE1n/VmJqOHV1Qm5od2l0/bEktN2Z0UDhKcjJR/MnFGR3dFUExLVVhC/MXZCWVhmbjRJPXcx/NDQwLWg4MTAtbi1u/dQ",
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  child: SizedBox(
-                    width: MediaQuery.widthOf(context),
+      body: SafeArea(child: pages.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: "Accueil",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Favoris"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "Catégorie",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
 
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Container(
-                          padding: EdgeInsetsGeometry.all(10),
-                          alignment: Alignment.topLeft,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text("en vente"),
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(horizontal: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Mission Impossible",
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                "Un agent secret doit sauver le monde d'une menace imminente. Action, suspense et rebondissements garantis. Ajoutez votre clé API",
-                              ),
-                            ],
-                          ),
-                        ),
-                        Container(
-                          padding: EdgeInsetsGeometry.all(10),
-                          width: double.infinity,
-                          child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text('Voir les détail'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: EdgeInsetsGeometry.symmetric(
-                horizontal: 20,
-                vertical: 5,
-              ),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Au cinéma maintenant"),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.arrow_forward_ios_sharp),
-                      ),
-                    ],
-                  ),
-                  ContainerImageCarousel(),
-                  SizedBox(height: 20),
-                  RowTextIcon(
-                    text: "Top 10 du moment",
-                    icon: Icons.arrow_forward_ios_sharp,
-                  ),
-                  SizedBox(height: 20),
-                  ContainerImageCarousel(),
-                  RowTextIcon(
-                    text: "Top 10 du moment",
-                    icon: Icons.arrow_forward_ios_sharp,
-                  ),
-                  ContainerImageCarousel(),
-                ],
-              ),
-            ),
-          ],
-        ),
+        onTap: _onItemTapped,
       ),
     );
   }
