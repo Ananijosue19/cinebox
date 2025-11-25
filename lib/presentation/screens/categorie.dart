d793428eb4fcd958826b0c25ccdbb41c79a8ebeb
josue-dex19 <ananijosue08@gmail.com>
2025-11-25 06:43:12 +0000

    feat: Display categories with icons in a grid
    Adds the `emoji_picker_flutter` dependency and implements a grid view on the `Categorie` screen to display a list of categories with corresponding icons.
    
    - Adds `emoji_picker_flutter: ^4.4.0` to `pubspec.yaml`.
    - Updates the `Categorie` screen to use a `GridView.builder`.
    - Populates the grid with a hardcoded list of 13 categories and their associated icons.
    - Styles each grid item with a grey background and rounded corners, displaying the category name and icon.

--- lib/presentation/screens/categorie.dart
+++ lib/presentation/screens/categorie.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 
 class Categorie extends StatelessWidget {
   const Categorie({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<String> categories = [
+      "Action",
+      "Comédie",
+      "Drame",
+      "Animation",
+      "Crime",
+      "Documentaire",
+      "Fantastique",
+      "Histoire",
+      "Horreur",
+      "Musical",
+      "Romance",
+      "Science-fiction",
+      "Thriller",
+    ];
+    final List<IconData> emojie = [
+      Icons.emoji_emotions_outlined,
+      Icons.emoji_events,
+      Icons.emoji_food_beverage,
+      Icons.emoji_nature,
+      Icons.emoji_objects,
+      Icons.emoji_people,
+      Icons.emoji_symbols,
+      Icons.emoji_transportation,
+      Icons.airplanemode_active,
+      Icons.emoji_objects,
+      Icons.emoji_people,
+      Icons.emoji_symbols,
+      Icons.emoji_transportation,
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Column(
@@ -23,6 +54,7 @@ class Categorie extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
         child: GridView.builder(
+          itemCount: 13,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             crossAxisSpacing: 10,
@@ -32,8 +64,12 @@ class Categorie extends StatelessWidget {
           itemBuilder: (context, index) {
             return Container(
               decoration: BoxDecoration(
-                color: Colors.red,
+                color: Colors.grey,
                 borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("${categories[index]}"), Icon(emojie[index])],
               ),
             );
           },
